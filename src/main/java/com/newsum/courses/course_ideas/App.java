d6ff36f8fd6a05c7370b8cc45bac4a4bc53cf7f2
Micah Newsum <mnewsum@glenair.com>
2021-01-24 13:07:58 -0800

    Add routig to sign-in.hbs

--- src/main/java/com/newsum/courses/course_ideas/App.java
+++ src/main/java/com/newsum/courses/course_ideas/App.java
@@ -1,5 +1,8 @@
 package com.newsum.courses.course_ideas;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import spark.ModelAndView;
 import spark.Spark;
 import spark.template.handlebars.HandlebarsTemplateEngine;
@@ -14,7 +17,12 @@ public class App
     public static void main( String[] args )
     {
     	Spark.get("/", (req, res) -> {return new ModelAndView(null,"index.hbs");}, new HandlebarsTemplateEngine());
-    	
-    	Spark.post("sign-in", (req, res) -> {return new ModelAndView(null,"sign-in.hbs");}, new HandlebarsTemplateEngine());
+    	Spark.post("/sign-in", (req, res) -> {
+									    		Map<String,String> model = new HashMap<>();
+										    	String userName = req.queryParams("username");
+										    	res.cookie("username", userName);
+												model.put("username", userName);
+    											return new ModelAndView(model,"sign-in.hbs");
+    										}, new HandlebarsTemplateEngine());
     }
 }
