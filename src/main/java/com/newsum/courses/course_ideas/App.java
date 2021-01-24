f016289b759a1ce047504c4f91ac36fd981a5077
Micah Newsum <mnewsum@glenair.com>
2021-01-24 11:09:08 -0800

    Add logic to handle post request method.

--- src/main/java/com/newsum/courses/course_ideas/App.java
+++ src/main/java/com/newsum/courses/course_ideas/App.java
@@ -14,5 +14,7 @@ public class App
     public static void main( String[] args )
     {
     	Spark.get("/", (req, res) -> {return new ModelAndView(null,"index.hbs");}, new HandlebarsTemplateEngine());
+    	
+    	Spark.post("sign-in", (req, res) -> {return new ModelAndView(null,"sign-in.hbs");}, new HandlebarsTemplateEngine());
     }
 }
