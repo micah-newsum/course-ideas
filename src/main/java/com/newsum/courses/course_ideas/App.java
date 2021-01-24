8ddb5096a921de98a1cc9556b2462b0c09f0dd34
Micah Newsum <mnewsum@glenair.com>
2021-01-24 11:00:02 -0800

    First commit

---
+++ src/main/java/com/newsum/courses/course_ideas/App.java
@@ -0,0 +1,18 @@
+package com.newsum.courses.course_ideas;
+
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Spark.get("/", (req, res) -> {return new ModelAndView(null,"index.hbs");}, new HandlebarsTemplateEngine());
+    }
+}
