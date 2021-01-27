d5cee0b446ad3e7431c4f333aecb39a7a146288a
Micah Newsum <mnewsum@glenair.com>
2021-01-26 20:56:52 -0800

    Add getters and setters to CourseIdea.

--- src/main/java/com/newsum/courses/model/CourseIdea.java
+++ src/main/java/com/newsum/courses/model/CourseIdea.java
@@ -10,4 +10,20 @@ public class CourseIdea
 		this.title = title;
 		this.username = username;
 	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 }
